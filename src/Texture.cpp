b7f8657b700d5db92c74bcf8cd7b9076da6677d7
guowj <1485765145@qq.com>
2019-12-28 07:36:19 +0800

    fix Texture bug

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -28,13 +28,15 @@ void Texture::initVertex(){
 	// 1.intput vertices
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW );
 
 	glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE, 8* sizeof(float), (void*)0);
@@ -44,12 +46,11 @@ void Texture::initVertex(){
     glVertexAttribPointer(2, 2,GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)) );
     glEnableVertexAttribArray(2);
    
-
     // // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-	// glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	// // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 	// // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-	// glBindVertexArray(0);	
+	glBindVertexArray(0);	
 }
 
 void Texture::initTexture(){
@@ -76,6 +77,7 @@ void Texture::initTexture(){
 
 void Texture::render(){
 
+    // printf("this is in Texture... \n");
     glBindTexture(GL_TEXTURE_2D ,texture);
 
     shader->use();
