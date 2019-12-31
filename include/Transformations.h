8365ed2ec5c36894439d98a9f56549bfc9ae7526
guoweijie <guoweijie004@ke.com>
2019-12-31 19:57:23 +0800

    添加glm头文件库

---
+++ include/Transformations.h
@@ -0,0 +1,33 @@
+#ifndef __TRANSFORMATIONS_H__
+#define __TRANSFORMATIONS_H__
+
+#include "./IOperator.h"
+#include "stb_image.h"
+#include "Shader.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include <GLFW/glfw3.h>
+
+
+class Transformation :public IOperator{
+	private:
+		void init();
+
+        unsigned int shaderProgram;
+        unsigned int VAO;
+        unsigned int VBO;
+        unsigned int EBO;
+        unsigned int texture1;
+        unsigned int texture2;
+        Shader * shader;
+
+	public:
+	 	void render();	
+		
+		void initShader();
+		void initVertex();
+        void initTexture();
+};
+
+#endif
