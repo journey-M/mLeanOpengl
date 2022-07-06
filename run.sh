2aff1b1983a66f1fdb69d51cb54ec4061d11329a
guoweijie <guoweijie@mail.360.cn>
2022-07-06 11:20:13 +0800

    修改运行脚本

--- run.sh
+++ run.sh
@@ -29,7 +29,8 @@ cmake ../CMakeLists.txt
 make -j6
 
 if [ $DEBUG -eq 1 ];then
-  (alacritty -e tmuxp load cppdebug -y) &
+  (alacritty --config-file ~/.config/alacritty/alacritty_debug.yml -e tmuxp load cppdebug -y) &
+  sleep 0.2
   (alacritty -e lldb glrun ) &
 else
   ./glrun &
